94cc928bbec551c88d73d1e8b7b846b7cc127077
Saša Todorović <sasa.todorovic@gmx.com>
2023-08-28 07:27:12 +0200

    Update instalacija.sh

--- instalacija.sh
+++ instalacija.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get install -y nano curl wget gzip bzip2 xz-utils pigz zip unzip cwltool make gcc g++ git tree
+sudo apt-get install -y nano curl wget gzip bzip2 xz-utils pigz zip unzip cwltool make gcc g++ git tree wamerican-small
