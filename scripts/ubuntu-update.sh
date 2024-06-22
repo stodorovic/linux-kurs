f91d4fe703a4edc446f7134dbd181de29593c34a
Saša Todorović <sasa.todorovic@gmx.com>
2024-06-22 15:51:36 +0200

    Update ubuntu-update.sh

--- scripts/ubuntu-update.sh
+++ scripts/ubuntu-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y nano curl wget gzip bzip2 xz-utils pigz zip unzip cwltool make gcc g++ git libxml-libxml-perl tree wamerican-small
+sudo apt-get install -y nano curl wget gzip bzip2 xz-utils pigz zip unzip cwltool make gcc g++ git libxml-libxml-perl tree wamerican-small dos2unix
 sudo apt-get upgrade -y
 
 sudo apt clean all -y
